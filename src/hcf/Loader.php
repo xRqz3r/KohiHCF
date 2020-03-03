45a8fdebc7afe1420f0abbdf09bc76eed5c0b82e
xRqz3r <60855569+xRqz3r@users.noreply.github.com>
2020-03-03 14:39:56 -0500

    Update Loader.php

--- src/hcf/Loader.php
+++ src/hcf/Loader.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace hcf;
 
+use hcf\managers\EntityManager;
 use hcf\provider\MySQLProvider;
 use pocketmine\plugin\PluginBase;
 
@@ -32,6 +33,11 @@ class Loader extends PluginBase {
 
     public function onEnable(): void {
         $this->provider = new MySQLProvider($this);
+    }
+    
+    public function getManagers(){
+        new EntityManager();
+        new CommandManager();
     }
 
     /**
